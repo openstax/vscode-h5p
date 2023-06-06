5da1ae31441a29448d6bf7171ad6cc5833c09b04
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-06-06 10:20:22 -0500

    Collapse props in Bloom and Dok

--- client/src/app/components/OpenStax/Blooms.tsx
+++ client/src/app/components/OpenStax/Blooms.tsx
@@ -1,28 +1,17 @@
 import BasicFormComponent from './BasicFormComponent';
 import { SingleDropdown } from './SingleDropdown';
+import { SingleInputProps } from './types';
 import { collect, range } from './utils';
 
 const options = collect(range(1, 7))
   .map((b) => b.toString())
   .map((b) => ({ label: b, value: b }));
 
-export default function BloomsDropdown({
-  handleInputChange,
-  value,
-}: {
-  handleInputChange: (v: string) => void;
-  value: string;
-}) {
+export default function BloomsDropdown(props: SingleInputProps) {
   return (
     <BasicFormComponent
       title={'Blooms'}
-      content={
-        <SingleDropdown
-          options={options}
-          handleInputChange={handleInputChange}
-          value={value}
-        />
-      }
+      content={<SingleDropdown options={options} {...props} />}
     />
   );
 }
