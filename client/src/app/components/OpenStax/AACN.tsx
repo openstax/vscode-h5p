3e6496121e772fd58fb367b50ac41a4c196da3f8
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-06-22 14:05:45 -0500

    Add new metadata fields for nursing books

---
+++ client/src/app/components/OpenStax/AACN.tsx
@@ -0,0 +1,25 @@
+import BasicFormComponent from './BasicFormComponent';
+import SingleInput from './SingleInput';
+import { SingleInputProps } from './types';
+
+const pattern = /^\d\.\d[a-z]$/;
+
+export default function AACN(props: SingleInputProps) {
+  const subProps = {
+    ...props,
+    handleInputChange: (value: string) => {
+      props.handleInputChange(value, !!value.match(pattern));
+    },
+  };
+
+  return (
+    <BasicFormComponent
+      title={'AACN'}
+      content={
+        <div className="col-12">
+          <SingleInput {...subProps} style={{ width: '100%' }} />
+        </div>
+      }
+    />
+  );
+}
