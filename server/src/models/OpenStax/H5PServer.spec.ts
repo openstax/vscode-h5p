799f3e707992cd57f8dff0dde0c4f4e236c1d8be
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-06-29 15:14:36 -0500

    Add some tests for openstax h5p server

---
+++ server/src/models/OpenStax/H5PServer.spec.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import Config from '../config';
+import OSH5PServer from './H5PServer';
+import request from 'supertest';
+import * as H5P from '@lumieducation/h5p-server';
+
+jest.mock('../H5PServer');
+
+describe('OSH5PServer', () => {
+  const config = new Config('', '');
+  const h5pConfig = new H5P.H5PConfig();
+  const app = express();
+  const mockEditor = {
+    config: h5pConfig,
+    contentStorage: {
+      saveOSMeta: jest.fn(),
+      getOSMeta: jest.fn(),
+    },
+  };
+
+  it('starts and does stuff', async () => {
+    const server: any = new OSH5PServer(jest.fn() as any, jest.fn() as any, '');
+    await server.start(mockEditor, app, config.port);
+    let res = await request(app).get('/does-not-exist-404-please');
+    expect(res.status).toBe(404);
+    expect(mockEditor.contentStorage.saveOSMeta).toBeCalledTimes(0);
+    expect(mockEditor.contentStorage.getOSMeta).toBeCalledTimes(0);
+
+    res = await request(app).get(
+      `${h5pConfig.baseUrl}/undefined/openstax-metadata/`
+    );
+    expect(res.status).toBe(200);
+    expect(res.body).toStrictEqual({});
+    expect(mockEditor.contentStorage.getOSMeta).toBeCalledTimes(1);
+
+    res = await request(app).post(`${h5pConfig.baseUrl}/1/openstax-metadata/`);
+    expect(res.status).toBe(200);
+    expect(mockEditor.contentStorage.saveOSMeta).toBeCalledTimes(1);
+  });
+});
