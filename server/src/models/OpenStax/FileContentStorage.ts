6e121960eb0672a9a411381a6248516c2016d92f
Chris Kline <58607806+ckline-tryptic@users.noreply.github.com>
2023-07-05 17:07:15 -0400

    separate private answers

--- server/src/models/OpenStax/FileContentStorage.ts
+++ server/src/models/OpenStax/FileContentStorage.ts
@@ -1,15 +1,25 @@
 import * as path from 'path';
 import * as fsExtra from 'fs-extra';
 import * as H5P from '@lumieducation/h5p-server';
+import * as fs from 'fs';
+import Config from '../config';
 
 const METADATA_NAME = 'metadata.json';
+const CONTENT_NAME = 'content.json';
 
-export default class OSStorage extends H5P.fsImplementations
-  .FileContentStorage {
+export default class OSStorage extends H5P.fsImplementations.FileContentStorage {
+
+  private privateContentDirectory
+
+  constructor(config: Config) {
+    super(config.contentDirectory);
+    this.privateContentDirectory = config.privateContentDirectory;
+  }
+
   protected async createContentId() {
     const idxOffset = 1;
     const numbered = fsExtra
-      .readdirSync(this.getContentPath(), { withFileTypes: true })
+      .readdirSync(this.contentPath, { withFileTypes: true })
       .filter((d) => d.isDirectory())
       .map((d) => parseInt(d.name))
       .filter((n) => !isNaN(n))
@@ -23,7 +33,7 @@ export default class OSStorage extends H5P.fsImplementations
   }
 
   public async addContent(
-    metadata: H5P.IContentMetadata,
+    osMeta: H5P.IContentMetadata,
     content: any,
     user: H5P.IUser,
     id?: string | undefined
@@ -32,18 +42,18 @@ export default class OSStorage extends H5P.fsImplementations
       id = await this.createContentId();
     }
     try {
-      await fsExtra.ensureDir(path.join(this.getContentPath(), id));
+      await fsExtra.ensureDir(path.join(this.contentPath, id));
       await this.writeJSON(
-        path.join(this.getContentPath(), id, 'h5p.json'),
-        metadata
+        path.join(this.contentPath, id, 'h5p.json'),
+        osMeta
       );
       await this.writeJSON(
-        path.join(this.getContentPath(), id, 'content.json'),
+        path.join(this.contentPath, id, 'content.json'),
         content
       );
     } catch (error) {
       /* istanbul ignore next */
-      await fsExtra.remove(path.join(this.getContentPath(), id));
+      await fsExtra.remove(path.join(this.contentPath, id));
       /* istanbul ignore next */
       throw new H5P.H5pError(
         'storage-file-implementations:error-creating-content'
@@ -66,18 +76,64 @@ export default class OSStorage extends H5P.fsImplementations
     return metadata;
   }
 
-  public async saveOSMeta(contentId: string, metadata: any) {
+  public async saveOSMeta(contentId: string, osMeta: any) {
+      // check if private
+    const privatePath = path.join(this.privateContentDirectory, contentId);
+    if (osMeta.isSolutionPublic === "false") {
+      const h5pPath = path.join(this.contentPath, contentId, CONTENT_NAME);
+
+      const h5p = await fsExtra.readJSON(h5pPath, 'utf8');
+
+      // move answer data to private folder
+      const privateData = {
+        answers: h5p.answers,
+      };
+
+      await fsExtra.ensureDir(privatePath);
+      await this.writeJSON(path.join(privatePath, 'content.json'), privateData);
+
+      // remove answers from public h5p object
+      delete h5p.answers;
+
+      // write h5p object back to h5p file
+      await this.writeJSON(h5pPath, h5p);
+    } else {
+      await fsExtra.ensureDir(privatePath);
+      await fsExtra.rm(privatePath, {"recursive": true});
+    }
+
     await this.writeJSON(
-      path.join(this.getContentPath(), contentId, METADATA_NAME),
-      metadata
+      path.join(this.contentPath, contentId, METADATA_NAME),
+      osMeta
     );
   }
 
   public async getOSMeta(contentId: string) {
-    const mdPath = path.join(this.getContentPath(), contentId, METADATA_NAME);
+    const mdPath = path.join(this.contentPath, contentId, METADATA_NAME);
     if (!fsExtra.existsSync(mdPath)) {
       return {};
     }
-    return await fsExtra.readJSON(mdPath);
+    // read metadata
+    // if private data exists, read it and add to file
+    const osMeta = await fsExtra.readJSON(mdPath);
+    if (osMeta.isSolutionPublic === "false") {
+      const privatePath = path.join(this.privateContentDirectory, contentId, CONTENT_NAME);
+      const privateData = await fsExtra.readJSON(privatePath);
+      osMeta.answers = privateData.answers;
+    }
+    return osMeta;
+  }
+
+  public async getParameters(contentId: string, user?: H5P.IUser | undefined): Promise<any> {
+    const content = await super.getParameters(contentId, user);
+    const osMeta = await this.getOSMeta(contentId);
+    if (osMeta.isSolutionPublic === "false") {
+      content.answers = osMeta.answers;
+    }
+    const params = {
+      ...osMeta,
+      ...content,
+    };
+    return params;
   }
 }
--- server/src/models/config.ts
+++ server/src/models/config.ts
@@ -2,10 +2,15 @@ export default class Config {
   constructor(
     public workspaceRoot: string,
     public contentPath: string = 'interactives',
+    public privatePath: string = 'private',
     public port: number = Number(process.env.PORT) || 27149
   ) {}
 
   public get contentDirectory() {
     return `${this.workspaceRoot}/${this.contentPath}`;
+  }
+
+  public get privateContentDirectory() {
+    return `${this.workspaceRoot}/${this.privatePath}/${this.contentPath}`;
   }
 }
