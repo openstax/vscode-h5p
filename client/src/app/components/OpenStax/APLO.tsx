7fcba67649d2fd6cbf30ac7e762debc666985507
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-08-09 11:50:13 -0500

    Update validation for ap-lo

--- client/src/app/components/OpenStax/APLO.tsx
+++ client/src/app/components/OpenStax/APLO.tsx
@@ -1,12 +1,28 @@
 import { InputSet, InputSetProps } from './InputSet';
 
-export default function APLO(props: InputSetProps) {
+const PATTERNS = {
+  'stax-apbio': {
+    pattern: /^[A-Z]{3}[-.]\d{1,2}[-.][A-Z]$/,
+    placeholder: '[A-Z]{3}-#{1,2}.[A-Z]',
+  },
+  'stax-apphys': {
+    pattern: /^\d[-.][A-Z][-.]\d{1,2}[-.]\d$/,
+    placeholder: '#.[A-Z].#{1,2}.#',
+  },
+  // TODO: stax-apush ?
+};
+
+export default function APLO(props: InputSetProps & { book: string }) {
+  const { pattern, placeholder } = PATTERNS[props.book] ?? {
+    pattern: /^[A-Z0-9.-]+$/,
+    placeholder: '',
+  };
   const subProps = {
     ...props,
     handleInputChange: (index: number, value: string) => {
-      props.handleInputChange(index, value, !!value.match(/\w+/));
+      props.handleInputChange(index, value, !!value.match(pattern));
     },
   };
 
-  return <InputSet title={'AP LO'} {...subProps} />;
+  return <InputSet title={'AP LO'} {...subProps} placeholder={placeholder} />;
 }
