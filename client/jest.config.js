1491f2144fecb952b7035dbe40f849404ea2176b
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-11-27 14:31:35 -0600

    Update client tests
    Needed to add jest-environment to account for some problems with jsdom

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: './jest-environment.js',
   roots: ['src'],
   coverageDirectory: './coverage/',
   collectCoverage: true,
