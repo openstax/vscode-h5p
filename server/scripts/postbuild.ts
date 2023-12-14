d53c87af0a68ae20b081e6d7e8ce1cf1179aba37
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-12-14 09:33:01 -0600

    Fix ckeditor skin
    The skin that was being used lacked certain icons.
    
    Copying this newer version's css and icons fixes the issue.

--- server/scripts/postbuild.ts
+++ server/scripts/postbuild.ts
@@ -15,37 +15,38 @@ import { assertValue } from '../../common/src/utils';
 
 const SERVER_ROOT = path.resolve(__dirname, '..');
 const OUT = path.resolve(SERVER_ROOT, 'out');
-const archiveFile =
+const ARCHIVE_FILE =
   process.argv[2] === undefined
     ? path.resolve(OUT, Config.h5pServerArchiveName)
     : path.resolve(process.argv[2]);
-const tempFolderPath = path.resolve(os.tmpdir(), 'h5p_builder');
-const nodeModules = path.resolve(SERVER_ROOT, 'node_modules');
-const config = new H5P.H5PConfig(undefined, Config.h5pConfig);
-const h5pEditor = createH5PEditor(
-  config,
-  `${tempFolderPath}/libraries`,
-  new Config(tempFolderPath, '', ''),
-  `${tempFolderPath}/temporary-storage`,
-  `${tempFolderPath}/user-data`,
+const TEMP_FOLDER = path.resolve(os.tmpdir(), 'h5p_builder');
+const NODE_MODULES = path.resolve(SERVER_ROOT, 'node_modules');
+const CONFIG = new H5P.H5PConfig(undefined, Config.h5pConfig);
+const H5P_EDITOR = createH5PEditor(
+  CONFIG,
+  `${TEMP_FOLDER}/libraries`,
+  new Config(TEMP_FOLDER, '', ''),
+  `${TEMP_FOLDER}/temporary-storage`,
+  `${TEMP_FOLDER}/user-data`,
   undefined,
   undefined,
   undefined,
 );
-const ckeditorPlugins = `${tempFolderPath}/editor/ckeditor/plugins`;
+const CKEDITOR_ROOT = path.resolve(TEMP_FOLDER, 'editor', 'ckeditor');
+const CKEDITOR_PLUGINS = path.resolve(CKEDITOR_ROOT, 'plugins');
 
-const registrationData = {
-  core_api_version: `${config.coreApiVersion.major}.${config.coreApiVersion.minor}`,
-  disabled: config.fetchingDisabled,
-  h5p_version: config.h5pVersion,
+const REGISTRATION_DATA = {
+  core_api_version: `${CONFIG.coreApiVersion.major}.${CONFIG.coreApiVersion.minor}`,
+  disabled: CONFIG.fetchingDisabled,
+  h5p_version: CONFIG.h5pVersion,
   local_id: machineIdSync(),
-  platform_name: config.platformName,
-  platform_version: config.platformVersion,
-  type: config.siteType,
-  uuid: config.uuid,
+  platform_name: CONFIG.platformName,
+  platform_version: CONFIG.platformVersion,
+  type: CONFIG.siteType,
+  uuid: CONFIG.uuid,
 };
 
-const user = new User();
+const USER = new User();
 
 function sh(cmd: string) {
   return new Promise((resolve, reject) => {
@@ -61,15 +62,15 @@ function sh(cmd: string) {
 
 async function downloadH5PLibs() {
   console.log('Updating H5P libraries...');
-  const response = await fetch(config.hubContentTypesEndpoint, {
+  const response = await fetch(CONFIG.hubContentTypesEndpoint, {
     method: 'POST',
-    body: qs.stringify(registrationData),
+    body: qs.stringify(REGISTRATION_DATA),
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
   const data = (await response.json()).contentTypes;
-  const librariesPath = `${tempFolderPath}/libraries`;
+  const librariesPath = `${TEMP_FOLDER}/libraries`;
   fsExtra.ensureDirSync(librariesPath);
   const existingLibraries = await Promise.all(
     fsExtra
@@ -106,7 +107,7 @@ async function downloadH5PLibs() {
     for (let tries = 3; tries--; tries > 0) {
       try {
         console.log(`Installing ${libraryName}`);
-        await h5pEditor.installLibraryFromHub(libraryName, user);
+        await H5P_EDITOR.installLibraryFromHub(libraryName, USER);
         break;
       } catch (e) {
         if (tries === 0) {
@@ -129,19 +130,22 @@ async function downloadH5PLibs() {
 }
 
 async function includePatchedMathtype() {
-  const mathTypePlugin = `${ckeditorPlugins}/ckeditor_wiris`;
+  const mathTypePlugin = `${CKEDITOR_PLUGINS}/ckeditor_wiris`;
   const patchFile = `${__dirname}/mathtype-plugin-js.patch`;
 
   console.log('Patching and including ckeditor_wiris plugin...');
-  fsExtra.ensureDirSync(ckeditorPlugins);
-  fsExtra.copySync(`${nodeModules}/@wiris/mathtype-ckeditor4/`, mathTypePlugin);
+  fsExtra.ensureDirSync(CKEDITOR_PLUGINS);
+  fsExtra.copySync(
+    `${NODE_MODULES}/@wiris/mathtype-ckeditor4/`,
+    mathTypePlugin,
+  );
   await sh(`patch "${mathTypePlugin}/plugin.js" "${patchFile}"`);
 }
 
-async function includeAdditionalCKEditorPlugins() {
-  const ckeditorPluginsRoot = path.resolve(SERVER_ROOT, 'ckeditor-plugins');
+function getCKEditorPluginPaths() {
+  const submodulesRoot = path.resolve(SERVER_ROOT, 'ckeditor-plugins');
   const ckEditorRepoPlugins = path.resolve(
-    ckeditorPluginsRoot,
+    submodulesRoot,
     'ckeditor4',
     'plugins',
   );
@@ -153,24 +157,38 @@ async function includeAdditionalCKEditorPlugins() {
     'indentblock',
     'indentlist',
     'iframe',
-  ].map((plugin) => path.resolve(ckEditorRepoPlugins, plugin));
-  pluginPaths.push(path.resolve(ckeditorPluginsRoot, 'codetag'));
-  pluginPaths.push(path.resolve(ckeditorPluginsRoot, 'insertpre'));
-  await Promise.all(
-    pluginPaths.map(async (src) => {
-      const pluginName = path.basename(src);
-      const dst = path.resolve(ckeditorPlugins, pluginName);
-      console.log(`Including ckeditor plugin "${pluginName}"`);
-      await fsExtra.copy(src, dst);
-    }),
+  ].map((plugin) => ({
+    src: path.resolve(ckEditorRepoPlugins, plugin),
+    dst: path.resolve(CKEDITOR_PLUGINS, plugin),
+  }));
+  pluginPaths.push({
+    src: path.resolve(submodulesRoot, 'codetag'),
+    dst: path.resolve(CKEDITOR_PLUGINS, 'codetag'),
+  });
+  pluginPaths.push({
+    src: path.resolve(submodulesRoot, 'insertpre'),
+    dst: path.resolve(CKEDITOR_PLUGINS, 'insertpre'),
+  });
+  pluginPaths.push(
+    ...['editor.css', 'icons.png'].map((file) => ({
+      src: path.resolve(
+        submodulesRoot,
+        'BootstrapCK4-Skin',
+        'skins',
+        'bootstrapck',
+        file,
+      ),
+      dst: path.resolve(CKEDITOR_ROOT, 'skins', 'bootstrapck', file),
+    })),
   );
+  return pluginPaths;
 }
 
 async function copyExtras() {
   const extras: Array<{ src: string; dst: string }> = [
     {
       src: path.resolve(
-        nodeModules,
+        NODE_MODULES,
         '@lumieducation',
         'h5p-server',
         'build',
@@ -183,10 +201,11 @@ async function copyExtras() {
       src: path.resolve(SERVER_ROOT, 'static'),
       dst: path.resolve(OUT, 'static'),
     },
+    ...getCKEditorPluginPaths(),
   ];
   await Promise.all(
     extras.map(async ({ src, dst }) => {
-      console.log(`Copying "${path.basename(src)}"`);
+      console.log(`Copying to output: "${path.relative(SERVER_ROOT, src)}"`);
       await fsExtra.copy(src, dst);
     }),
   );
@@ -209,8 +228,8 @@ async function createArchive(archiveFile: string, wd: string, files: string[]) {
 
 async function main() {
   // Extract archive if it exists
-  if (fsExtra.pathExistsSync(archiveFile)) {
-    await extractArchive(archiveFile, tempFolderPath, false, undefined, {
+  if (fsExtra.pathExistsSync(ARCHIVE_FILE)) {
+    await extractArchive(ARCHIVE_FILE, TEMP_FOLDER, false, undefined, {
       strip: 0,
     });
   }
@@ -221,10 +240,9 @@ async function main() {
       : Promise.resolve(),
     copyExtras(),
     includePatchedMathtype(),
-    includeAdditionalCKEditorPlugins(),
   ]);
   // Recreate it
-  await createArchive(archiveFile, tempFolderPath, ['libraries', 'editor']);
+  await createArchive(ARCHIVE_FILE, TEMP_FOLDER, ['libraries', 'editor']);
 }
 
 main()
@@ -233,5 +251,5 @@ main()
   })
   .finally(() => {
     console.log('Cleaning up temporary directory...');
-    fsExtra.rmSync(tempFolderPath, { recursive: true, force: true });
+    fsExtra.rmSync(TEMP_FOLDER, { recursive: true, force: true });
   });
