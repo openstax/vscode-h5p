8022be95fa12ea7757a5bd4f2fc77e726bcce406
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-01-18 14:13:13 -0600

    Parse all content strings as HTML instead of a subset
    Should prevent errors related to temporary images not being replaced
    
    Example: 'ERROR: Found unexpected temp path in ...'

--- server/src/models/OpenStax/ContentMutators.ts
+++ server/src/models/OpenStax/ContentMutators.ts
@@ -12,7 +12,7 @@ interface ContentField {
 
 interface HTMLContentField extends ContentField {
   value: string;
-  parent: Partial<unknown>;
+  parent: Partial<unknown> | undefined;
   type: 'string';
 }
 
@@ -23,15 +23,7 @@ export interface HTMLContent {
 }
 
 function isHTMLField(field: ContentField): field is HTMLContentField {
-  const { type, name, parent, fqPath } = field;
-  const parentName = fqPath[fqPath.length - 2];
-  if (type !== 'string' || parent === undefined) return false;
-  return (
-    name === 'text' ||
-    name === 'question' ||
-    name === 'tip' ||
-    (parentName === 'questions' && Array.isArray(parent))
-  );
+  return typeof field.value === 'string' && field.value.length > 0;
 }
 
 export function parseAsHTML(value: string): HTMLContent {
@@ -94,7 +86,7 @@ export function iterHTML(
   }) => void,
 ) {
   iterContent(content, (field) => {
-    if (isHTMLField(field)) {
+    if (isHTMLField(field) && field.parent !== undefined) {
       const document = parseAsHTML(field.value);
       handler({ fieldName: field.name, fqPath: field.fqPath, document });
       Reflect.set(field.parent, field.name, document.serialize());
