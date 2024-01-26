2a41e3821779fcc967b69c89b6310880f5188dce
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-01-26 17:00:47 -0600

    Add button to toggle preview for CollaboratorSolution
    It does render mathml  when the preview is toggled

--- client/src/app/components/OpenStax/CollaboratorSolution.tsx
+++ client/src/app/components/OpenStax/CollaboratorSolution.tsx
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import BasicFormComponent from './BasicFormComponent';
 import { SingleInputProps } from './types';
 
 export default function CollaboratorSolution(
   props: SingleInputProps & { title: string },
 ) {
-  // TODO: Maybe add support for previewing HTML/Math
-  // const [isPreview, setIsPreview] = useState(false);
+  const [isPreview, setIsPreview] = useState(false);
   return (
     <BasicFormComponent
       {...props}
       content={
-        <div className="col-12">
-          <textarea
-            rows={3}
-            value={props.value}
-            onChange={(e) => props.handleInputChange(e.target.value)}
-            style={{ width: '100%' }}
-          />
+        <div className="col-12" style={{ position: 'relative' }}>
+          {isPreview ? (
+            <div dangerouslySetInnerHTML={{ __html: props.value }}></div>
+          ) : (
+            <textarea
+              data-control-type="textarea"
+              rows={3}
+              value={props.value}
+              onChange={(e) => props.handleInputChange(e.target.value)}
+              style={{ width: '100%' }}
+            />
+          )}
+          <button
+            data-control-type="preview-button"
+            style={{
+              position: 'absolute',
+              bottom: '15px',
+              right: '30px',
+              width: '75px',
+              fontSize: '12pt',
+              fontFamily: 'arial',
+            }}
+            onClick={() => setIsPreview(!isPreview)}
+          >
+            Preview
+          </button>
         </div>
       }
     />
