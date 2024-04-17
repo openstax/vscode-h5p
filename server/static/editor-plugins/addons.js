3f9d40ebdc6d7b9c1b9ad05eacf08df4fc266360
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-04-17 16:08:08 -0500

    Fix the MathType editor's position on the screen
    If multiple instances are opened, they are stacked vertically.
    
    This also moves the math source editor (wrs_code) in the same manner.
    
    Multiple h5p editor instances can be opened at once. Each h5p editor instance
    has its own iframe.
    
    The custom viewportUpdate event is sent to each iframe.
    
    Consequently, when multiple h5p editors are opened and each has one or more
    MathType editor instances open, all MathType editors should be updated within
    their respective iframes.
    
    Additionally, the MathType editor instances should remain contained within
    their respective iframes (see `top` in addons.js).

--- server/static/editor-plugins/addons.js
+++ server/static/editor-plugins/addons.js
@@ -1,10 +1,99 @@
 // https://h5p.org/adding-text-editor-buttons
 
+function getFixMathTypeDialog() {
+  let offsetTop = 0;
+  let viewportHeight = 0;
+  let parentOffsetTop = 0;
+  let parentWidth = 0;
+  let parentHeight = 0;
+  let mathTypeInstances = [];
+  let waitingForUpdate = false;
+
+  const setMathTypePosition = () => {
+    const visibleInstances = mathTypeInstances.filter(
+      (editor) => !editor.classList.contains('wrs_closed')
+    );
+    if (waitingForUpdate || visibleInstances.length === 0) return;
+    waitingForUpdate = true;
+    requestAnimationFrame(() => {
+      try {
+        const boundingRects = visibleInstances
+          .map((dialog) => dialog.getBoundingClientRect())
+        const instanceHeights = boundingRects.map((rect) => rect.height)
+        const totalHeight = instanceHeights
+          .reduce((ax, x) => ax + x);
+        const startingOffsetTop =
+          offsetTop - parentOffsetTop + viewportHeight / 2 - totalHeight / 2;
+        let relOffsetTop = 0;
+        visibleInstances.forEach((dialog, idx) => {
+          const dialogWidth = boundingRects[idx].width;
+          const top = Math.min(
+            Math.max(startingOffsetTop, 0), parentHeight - totalHeight
+          ) + relOffsetTop;
+          const left = Math.max((parentWidth / 2 - dialogWidth / 2), 0);
+          relOffsetTop += instanceHeights[idx];
+          dialog.style.inset = `${top}px ${left}px 0px`;
+        });
+      } catch (e) {
+        console.error(e);
+      } finally {
+        waitingForUpdate = false;
+      }
+    });
+  }
+
+  const isMathTypeNode = (node) =>
+    node.id === 'wrs_code' || node.id?.startsWith('wrs_modal_dialogContainer')
+  const observe = new MutationObserver(function (mutations) {
+    const addedOrRemovedDialog = mutations.some(
+      (mutation) =>
+        Array.from(mutation.addedNodes).some(isMathTypeNode) ||
+        Array.from(mutation.removedNodes).some(isMathTypeNode)
+    );
+    const shouldUpdate = addedOrRemovedDialog || (
+      mathTypeInstances.length > 0 &&
+      // Look for cases where a dialog was resized or hidden
+      mutations
+        .some((mutation) =>
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'class' &&
+          mathTypeInstances.some((instance) => mutation.target === instance)
+        )
+    );
+    if (addedOrRemovedDialog) {
+      mathTypeInstances = Array.from(
+        document.querySelectorAll('#wrs_code, .wrs_modal_dialogContainer')
+      );
+    }
+    // Move the dialogs when they are added, hidden/removed, or resized
+    if (shouldUpdate) {
+      setMathTypePosition();
+    }
+  });
+
+  observe.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+  });
+
+  return function(e) {
+    offsetTop = e.detail.parentViewport.pageTop;
+    viewportHeight = e.detail.parentViewport.height;
+    parentOffsetTop =
+      e.detail.boundingClientRect.top + e.detail.parentViewport.pageTop;
+    parentWidth = e.detail.boundingClientRect.width;
+    parentHeight = e.detail.boundingClientRect.height;
+    setMathTypePosition();
+  }
+}
+
 (function ($) {
   $(document).ready(function () {
     if (!window.CKEDITOR) {
       return;
     }
+    document.addEventListener('viewportUpdate', getFixMathTypeDialog())
 
     // Register our plugin
     H5PEditor.HtmlAddons = H5PEditor.HtmlAddons || {};
